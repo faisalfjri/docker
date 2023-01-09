ed50bcd2cbf335c9a5fbdfa92c16960a1c95458c
faisalfjri <faisalfjrii@gmail.com>
2023-01-09 12:18:23 +0700

    update docker

--- src/buatsurat/frontend/server.js
+++ src/buatsurat/frontend/server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-const path = require('path')
 
 // Constants
 const PORT = 3001;
@@ -9,12 +8,9 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')))
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+app.get('/', (req, res) => {
+  res.send('Buatsurat Frontend');
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
